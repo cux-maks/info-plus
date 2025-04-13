527de04ed56606c7baa7ce111e9bf997924b6164
Jeongminseo <112807942+minseo0810@users.noreply.github.com>
2025-04-13 15:11:49 +0900

    ✨feat: Employee 모델 추가 및 __all__ 목록 업데이트

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,6 @@ from .category import Category
 from .feature import Feature
 from .user_category import UserCategory
 from .users import Users
+from .employee import Employee
 
-__all__ = ["Base", "Category", "Feature", "UserCategory", "Users"]
+__all__ = ["Base", "Category", "Feature", "UserCategory", "Users", "Employee"]
