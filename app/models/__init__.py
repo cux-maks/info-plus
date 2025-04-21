befe19a47c4e27bd46a68601c6ebf0d161b1a60c
cux-maks <volcano020716@gmail.com>
2025-04-21 23:17:02 +0900

    ✨refactor: User 모델을 __all__에서 제거하여 불필요한 의존성 제거

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,8 +3,7 @@ from .category import Category
 from .employee import Employee
 from .feature import Feature
 from .news import News
-from .user import User
 from .user_category import UserCategory
 from .users import Users
 
-__all__ = ["Base", "Category", "Feature", "UserCategory", "Users", "Employee", "News", "User"]
+__all__ = ["Base", "Category", "Feature", "UserCategory", "Users", "Employee", "News"]
